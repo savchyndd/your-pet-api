de03b3cd746365bb8fea814b0d2068d6824f7222
Dmytro <savchyndd@gmail.com>
2023-08-20 18:10:44 +0200

    add get notes

--- src/constants/noticeConstants.js
+++ src/constants/noticeConstants.js
@@ -1,14 +1,22 @@
 const CATEGORY = [
   "sell",
-  "lost-found",
-  "in-good-hands",
+  "lost/found",
+  "in good hands",
   "favorite ads",
   "my ads",
+];
+const CATEGORY_FOR_PARAMS = [
+  "sell",
+  "lost-found",
+  "in-good-hands",
+  "favorite-ads",
+  "my-ads",
 ];
 
 const SEX = ["male", "female"];
 
 module.exports = {
   CATEGORY,
+  CATEGORY_FOR_PARAMS,
   SEX,
 };
