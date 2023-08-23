4cb4856eefb358b4d78966f6fe7ffb30aee596c1
IgorPetrenko72 <i.v.petrenko@gmail.com>
2023-08-23 12:09:51 +0200

    api mistake

--- src/controllers/pet/addAvatar.js
+++ src/controllers/pet/addAvatar.js
@@ -1,10 +1,24 @@
+const path = require("path");
+const { rename } = require("fs/promises");
 const { tryCatchWrapper } = require('../../helpers/index');
 const { MyPet } = require('../../models/pet');
+const Jimp = require("jimp");
+
+const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
 
 const addAvatar = async (req, res) => {
-  const { petId } = req.params;
-    const { path } = req.file;
-    const avatarURL = path;
+  // const { petId } = req.params;
+  // const { path } = req.file;
+  const avatarName = `${_id}_${filename}`;
+  const resultUpload = path.join(avatarsDir, avatarName);
+  await rename(tempUpload, resultUpload);
+  Jimp.read(resultUpload, (err, avatar) => {
+    if (err) throw err;
+    avatar.resize(250, 250).write(resultUpload);
+  });
+  // const avatarURL = path.join("avatars", avatarName);
+
+  // const avatarURL = path;
 
   await MyPet.findByIdAndUpdate(petId, { avatarURL });
 
