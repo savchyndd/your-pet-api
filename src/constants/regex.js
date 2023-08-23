376b88d8c7ff7e4956be5a8713af877e3509d933
Dmytro <savchyndd@gmail.com>
2023-08-23 11:54:11 +0200

    fix

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -1,14 +1,18 @@
 const NAME_REGEX = /^[a-zA-Z]{2,16}$/;
 
 const EMAIL_REGEX =
+  // eslint-disable-next-line no-control-regex
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,16}$/;
 
 const BIRTHDAY_REGEX =
+  // eslint-disable-next-line no-useless-escape
   /^([0-2][0-9]|(3)[0-1])(\-)(((0)[0-9])|((1)[0-2]))(\-)\d{4}$/;
 
 const PHONE_NUMBER_REGEX = /^[+][0-9]{1,4}[0-9]{4,}$/;
+
+const LOCATION_REGEX = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
 
 module.exports = {
   NAME_REGEX,
@@ -16,4 +20,5 @@ module.exports = {
   PASSWORD_REGEX,
   BIRTHDAY_REGEX,
   PHONE_NUMBER_REGEX,
+  LOCATION_REGEX,
 };
--- src/models/notice.js
+++ src/models/notice.js
@@ -2,79 +2,86 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
 const { handleMongooseError } = require("../helpers");
-const { NAME_REGEX, BIRTHDAY_REGEX } = require("../constants/regex");
+const {
+  NAME_REGEX,
+  BIRTHDAY_REGEX,
+  LOCATION_REGEX,
+} = require("../constants/regex");
 const { SEX, CATEGORY } = require("../constants/noticeConstants");
 const isHasRequireCategory = require("../utils/isHasRequireCategory");
 
-const noticeSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, "title is required"],
-  },
-  name: {
-    type: String,
-    match: NAME_REGEX,
-    required: [true, "name is required"],
-  },
-  birthday: {
-    type: String,
-    match: BIRTHDAY_REGEX,
-    required: [true, "birthday is required"],
-  },
-  type: {
-    type: String,
-    match: NAME_REGEX,
-    required: [true, "birthday is required"],
-  },
-  petAvatarURL: {
-    type: String,
-    required: [true, "birthday is required"],
-  },
-  place: {
-    type: String,
-    match: NAME_REGEX,
-    default: "",
-    required: isHasRequireCategory(this.category, "place"),
-  },
-  sex: {
-    type: String,
-    enum: {
-      values: [...SEX],
-      message: `Have only ${SEX.join(", ")}`,
+const noticeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "title is required"],
     },
-    default: SEX[0],
-    required: isHasRequireCategory(this.category, "sex"),
-  },
-  comments: {
-    type: String,
-    max: [120, "Comments shoud be less 120 symbols"],
-    default: "",
-  },
-  category: {
-    type: String,
-    enum: {
-      values: [...CATEGORY],
-      message: `Have only ${CATEGORY.join(", ")}`,
+    petAvatarURL: {
+      type: String,
+      required: [true, "petAvatarURL is required"],
     },
-    default: CATEGORY[0],
-    required: [true, "category is required"],
-  },
-  price: {
-    type: Number,
-    min: [1, "Price must be more than 0"],
-    required: isHasRequireCategory(this.category, "price"),
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "users",
-  },
-  idUsersAddedFavorite: [
-    {
+    name: {
+      type: String,
+      match: NAME_REGEX,
+      required: [true, "name is required"],
+    },
+    birthday: {
+      type: String,
+      match: BIRTHDAY_REGEX,
+      required: [true, "birthday is required"],
+    },
+    type: {
+      type: String,
+      match: NAME_REGEX,
+      required: [true, "type is required"],
+    },
+    sex: {
+      type: String,
+      enum: {
+        values: [...SEX],
+        message: `Have only ${SEX.join(", ")}`,
+      },
+      default: SEX[0],
+      required: isHasRequireCategory(this.category, "sex"),
+    },
+    comments: {
+      type: String,
+      max: [120, "Comments shoud be less 120 symbols"],
+      default: "",
+    },
+    location: {
+      type: String,
+      match: LOCATION_REGEX,
+      default: "",
+      required: isHasRequireCategory(this.category, "location"),
+    },
+    category: {
+      type: String,
+      enum: {
+        values: [...CATEGORY],
+        message: `Have only ${CATEGORY.join(", ")}`,
+      },
+      default: CATEGORY[0],
+      required: [true, "category is required"],
+    },
+    price: {
+      type: Number,
+      min: [1, "Price must be more than 0"],
+      required: isHasRequireCategory(this.category, "price"),
+    },
+    owner: {
       type: Schema.Types.ObjectId,
       ref: "users",
     },
-  ],
-});
+    idUsersAddedFavorite: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
+  },
+  { versionKey: false, timestamps: true }
+);
 
 noticeSchema.post("save", handleMongooseError);
 const Notice = model("notices", noticeSchema);
